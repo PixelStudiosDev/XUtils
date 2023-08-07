fd7d9bc0b6dd5cd86c6f27866a4af0a6b2aebaca
CubeCrafter72 <jacopociet@gmail.com>
2023-08-07 17:13:39 +0200

    update

--- src/main/java/me/cubecrafter/xutils/HttpUtil.java
+++ src/main/java/me/cubecrafter/xutils/HttpUtil.java
@@ -2,7 +2,6 @@ package me.cubecrafter.xutils;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.sun.net.httpserver.Headers;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.UtilityClass;
 
@@ -12,7 +11,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -20,20 +19,21 @@ import java.util.concurrent.CompletableFuture;
 public class HttpUtil {
 
     public static CompletableFuture<Response> get(String url) {
-        return get(url, new Headers());
+        return get(url, new HashMap<>());
     }
 
-    public static CompletableFuture<Response> get(String url, Headers headers) {
+    public static CompletableFuture<Response> get(String url, Map<String, String> headers) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+
                 connection.setRequestMethod("GET");
                 connection.setConnectTimeout(5000);
                 connection.setReadTimeout(5000);
                 connection.setRequestProperty("User-Agent", "XUtils/1.0");
-                for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
-                    connection.setRequestProperty(entry.getKey(), entry.getValue().get(0));
-                }
+
+                headers.forEach(connection::setRequestProperty);
+
                 return new Response(connection.getResponseCode(), read(connection.getInputStream()));
             } catch (IOException e) {
                 e.printStackTrace();
@@ -43,23 +43,25 @@ public class HttpUtil {
     }
 
     public static CompletableFuture<Response> post(String url, JsonObject body) {
-        return post(url, body, new Headers());
+        return post(url, body, new HashMap<>());
     }
 
-    public static CompletableFuture<Response> post(String url, JsonObject body, Headers headers) {
+    public static CompletableFuture<Response> post(String url, JsonObject body, Map<String, String> headers) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+
                 connection.setRequestMethod("POST");
                 connection.setConnectTimeout(5000);
                 connection.setReadTimeout(5000);
                 connection.setRequestProperty("User-Agent", "XUtils/1.0");
                 connection.setRequestProperty("Content-Length", String.valueOf(body.toString().length()));
-                for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
-                    connection.setRequestProperty(entry.getKey(), entry.getValue().get(0));
-                }
+
+                headers.forEach(connection::setRequestProperty);
+
                 connection.setDoOutput(true);
                 connection.getOutputStream().write(body.toString().getBytes());
+
                 return new Response(connection.getResponseCode(), read(connection.getInputStream()));
             } catch (IOException e) {
                 e.printStackTrace();
