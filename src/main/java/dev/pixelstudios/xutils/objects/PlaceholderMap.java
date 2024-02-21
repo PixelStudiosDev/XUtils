70435e0a5819c1d9c11c76cfe365e0a93a817360
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2024-02-21 16:35:11 +0100

    1.6.4

--- src/main/java/dev/pixelstudios/xutils/objects/PlaceholderMap.java
+++ src/main/java/dev/pixelstudios/xutils/objects/PlaceholderMap.java
@@ -1,5 +1,7 @@
 package dev.pixelstudios.xutils.objects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +11,7 @@ import java.util.stream.Collectors;
 public class PlaceholderMap implements Cloneable {
 
     private final Map<String, Supplier<String>> placeholders = new HashMap<>();
+    private final Map<String, Supplier<List<String>>> multiLinePlaceholders = new HashMap<>();
 
     public PlaceholderMap add(String key, Supplier<String> value) {
         placeholders.put(key, value);
@@ -20,6 +23,16 @@ public class PlaceholderMap implements Cloneable {
         return this;
     }
 
+    public PlaceholderMap addMultiple(String key, Supplier<List<String>> value) {
+        multiLinePlaceholders.put(key, value);
+        return this;
+    }
+
+    public PlaceholderMap addMultiple(String key, List<String> value) {
+        multiLinePlaceholders.put(key, () -> value);
+        return this;
+    }
+
     public String parse(String text) {
         for (Map.Entry<String, Supplier<String>> entry : placeholders.entrySet()) {
             text = text.replace(entry.getKey(), entry.getValue().get());
@@ -28,13 +41,34 @@ public class PlaceholderMap implements Cloneable {
     }
 
     public List<String> parse(List<String> text) {
-        return text.stream().map(this::parse).collect(Collectors.toList());
+        List<String> parsed = new ArrayList<>();
+
+        for (String line : text) {
+            boolean found = false;
+
+            for (Map.Entry<String, Supplier<List<String>>> entry : multiLinePlaceholders.entrySet()) {
+                if (line.contains(entry.getKey())) {
+                    parsed.addAll(entry.getValue().get());
+
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                parsed.add(parse(line));
+            }
+        }
+        return text;
     }
 
     @Override
     public PlaceholderMap clone() {
         PlaceholderMap map = new PlaceholderMap();
+
         map.placeholders.putAll(this.placeholders);
+        map.multiLinePlaceholders.putAll(this.multiLinePlaceholders);
+
         return map;
     }
 
