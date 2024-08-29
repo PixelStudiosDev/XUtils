458e21720b90ba2c3a5bd56127ad6cbf783acdd3
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2024-08-29 13:19:24 +0200

    1.9.9

---
+++ src/main/java/dev/pixelstudios/xutils/objects/TriConsumer.java
@@ -0,0 +1,19 @@
+package dev.pixelstudios.xutils.objects;
+
+import java.util.Objects;
+
+@FunctionalInterface
+public interface TriConsumer<T, U, V> {
+
+    void accept(T t, U u, V v);
+
+    default TriConsumer<T, U, V> andThen(TriConsumer<? super T, ? super U, ? super V> after) {
+        Objects.requireNonNull(after);
+
+        return (l, r, v) -> {
+            accept(l, r, v);
+            after.accept(l, r, v);
+        };
+    }
+
+}
