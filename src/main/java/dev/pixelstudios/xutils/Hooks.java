94ce5509c6c5ff5fe6d600dc5eda1a912a0fd0da
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2024-09-22 13:20:58 +0200

    update XSeries

---
+++ src/main/java/dev/pixelstudios/xutils/Hooks.java
@@ -0,0 +1,40 @@
+package dev.pixelstudios.xutils;
+
+import dev.pixelstudios.xutils.text.TextUtil;
+import lombok.experimental.UtilityClass;
+import org.bukkit.Bukkit;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@UtilityClass
+public class Hooks {
+
+    private final Set<String> HOOKS = new HashSet<>();
+
+    public boolean register(String plugin, Runnable function) {
+        if (!Bukkit.getPluginManager().isPluginEnabled(plugin) || HOOKS.contains(plugin)) {
+            return false;
+        }
+
+        HOOKS.add(plugin);
+        function.run();
+
+        TextUtil.info("Hooked into " + plugin + ".");
+
+        return true;
+    }
+
+    public boolean isRegistered(String plugin) {
+        return HOOKS.contains(plugin);
+    }
+
+    public void unregister(String plugin) {
+        HOOKS.remove(plugin);
+    }
+
+    public void unregisterAll() {
+        HOOKS.clear();
+    }
+
+}
